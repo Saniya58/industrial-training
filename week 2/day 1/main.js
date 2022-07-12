c3fbd5a8b2ddcf1bf50b93782539300dd21e530e
saniya58 <sohelshk1858@gmail.com>
2022-07-12 14:07:26 +0530

    add:file

---
+++ week 2/day 1/main.js
@@ -0,0 +1,13 @@
+function create(){
+
+
+let email=document.getElementById("email1").Value;
+let studentname=document.getElementById("name").Value;
+let clg=document.getElementById("clg1").value;
+let branch=document.getElementById("branch1").Value;
+let p=document.getElementById("phone1").Value;
+let address=document.getElementById("address").value;
+
+
+document.write(email+ "<br/>" + studentname+ "<br/>" + branch +"<br/>"+ p +"<br/>" + address+ "<br/>" + clg );
+}
